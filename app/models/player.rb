51e5723c3fbb9e2a0937720dcda19aa1afae3916
Matthew Haney <mahaney@q.com>
2013-03-12 14:41:50 -0600

    * Model work

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,6 +1,7 @@
 class Player < ActiveRecord::Base
-  attr_accessible :name, :password, :secret, :tagged, :tags, :year
-  serialize :tags
-  has_one :target
+  attr_accessible :email, :name, :password, :secret, :tagged, :tags, :year
+  # Array of players tagged
+  has_one :target, Player
+  has_many :tags
   belongs_to :game
 end
